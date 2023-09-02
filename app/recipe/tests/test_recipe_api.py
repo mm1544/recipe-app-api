60b1cf4ec851e95c8d15c4298495645231bfd4ba
mminskis <martynas.minskis@jtrs.co.uk>
2023-09-02 16:34:56 +0100

    Add test for creating recipes

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -119,3 +119,29 @@ class PrivateRecipeApiTests(TestCase):
         # It is just one recipe so we don't pass-in many=True
         serializer = RecipeDetailSerializer(recipe)
         self.assertEqual(res.data, serializer.data)
+
+    def test_create_recipe(self):
+        """Test creating a recipe."""
+        # Not using 'create_recipe' helper f-n because the whole \
+        # pont of this test is creating recipe through API.
+
+        # Will pass a payload through API with the content of \
+        # a recipe. We want to ensure that the recipe was \
+        # created successfuly and corectly in the database.
+        payload = {
+            'title': 'Sample recipe',
+            'time_minutes': 30,
+            'price': Decimal('5.99'),
+        }
+        res = self.client.post(RECIPIES_URL, payload)  # /api/recipies/recipe
+
+        # 201 - created HTTP responce code.
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        recipe = Recipe.objects.get(id=res.data['id'])
+        # It will iterate trough payload and 'k' will be key, and 'v' will \
+        # be value.
+        for k, v in payload.items():
+            # 'getattr'(!) - Python f-n. It allows to get an attribute without using \
+            # 'dot' notation(!!!).
+            self.assertEqual(getattr(recipe, k), v)
+        self.assertEqual(recipe.user, self.user)
